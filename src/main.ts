8bf87407e94654a6c3502c894a7c34ea6c13f183
Peter Lamberg <pgithub@pelam.fi>
2019-12-09 13:56:24 +0200

    Fix imports in main.ts, misc links to README.md

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,29 @@
-import { delayedHello } from './DelayedHello';
+import * as AWS from 'aws-sdk'
 
-import AWS from 'aws-sdk'
+console.log("Script starting");
 
-console.log("Hello world!");
+AWS.config.setPromisesDependency(null); 
 
 export interface ManagementScriptConfig {
   workmailOrganizationId: string
 }
 
-import fs from 'fs';
+import {readFileSync} from 'fs';
 
 export function loadConfiguration(): ManagementScriptConfig {
-  // https://stackoverflow.com/a/52591988/1148030
-  const data = fs.readFileSync(".management-script-config.json")
+  const data = readFileSync("./management-script-config.json")
   return JSON.parse(data.toString()) as ManagementScriptConfig
 }
 
-// https://github.com/aws-samples/aws-nodejs-sample#basic-configuration
-// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-json-file.html
+const workmail = new AWS.WorkMail({endpoint: "https://workmail.eu-west-1.amazonaws.com"});
 
-// https://github.com/aws-samples/aws-nodejs-sample/blob/master/sample.js
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/WorkMail.html
-// https://docs.aws.amazon.com/workmail/latest/APIReference/API_AssociateMemberToGroup.html
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/WorkMail.html
-// https://docs.aws.amazon.com/code-samples/latest/catalog/code-catalog-typescript.html
-const workmail = new AWS.WorkMail({endpoint: "workmail.undefined.amazonaws.com"});
 workmail.config.loadFromPath("./aws-sdk-config.json")
 
-const config = loadConfiguration()
+const scriptConfig = loadConfiguration()
 
-async function foo() {
-  const foo = await workmail.listUsers({OrganizationId: config.workmailOrganizationId}).promise()
+async function main() {
+  const foo = await workmail.listUsers({OrganizationId: scriptConfig.workmailOrganizationId}).promise()
   console.log(foo)
 }
 
-
-AWS.config.setPromisesDependency(null); 
-
-
-async function asyncWrapper() {
-  const hello = await delayedHello("delayed");
-  console.log(hello)
-}
-
-asyncWrapper();
-foo();
+main()
