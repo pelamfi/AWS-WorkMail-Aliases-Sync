512a2af61c500058cc71be2b08210e5c11b62450
Peter Lamberg <pgithub@pelam.fi>
2019-12-15 14:56:20 +0200

    Aliases file parsing WIP

--- __tests__/AliasesFileParse.spec.ts
+++ __tests__/AliasesFileParse.spec.ts
@@ -1,7 +1,20 @@
-import {parse} from '../src/AliasesFileParse';
+import { parse, ParseError } from '../src/AliasesFileParse';
 
 describe('Parsing aliases file', () => {
   it('accepts empty file', () => {
-    expect(parse("")).toStrictEqual({aliases:[]});
+    expect(parse("")).toStrictEqual({ aliases: [] });
   });
+
+  it('accepts single alias', () => {
+    expect(parse("foo: bar")).toStrictEqual({ aliases: [{ localEmail: "foo", targets: ["bar"] }] });
+  });
+
+  it('skips comments and empty lines', () => {
+    expect(parse("#comment\nfoo: bar\n\n")).toStrictEqual({ aliases: [{ localEmail: "foo", targets: ["bar"] }] });
+  });
+
+  it('detects error', () => {
+    expect(parse("\nunknown stuff!!!\nfoo: bar\n\n")).toStrictEqual(new ParseError("Unrecognized aliases file line: unknown stuff!!!"));
+  });
+
 });
--- src/AliasesFileParse.ts
+++ src/AliasesFileParse.ts
@@ -1,12 +1,53 @@
 
 export interface Alias {
+  localEmail: string
+  targets: string[]
 }
 
 export interface AliasesFile {
   aliases: Alias[]
 }
 
-export function parse(input: string): AliasesFile {
-    console.log(input)
-    return {aliases:[]}
+export class ParseError {
+  error: string
+
+  constructor(error: string) {
+    this.error = error;
+  }
+}
+
+const aliasRegex = /^\s*([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+)\s*:\s*([-a-z0-9A-Z_,\s]+)\s*$/
+const commentRegex = /^\s*#.*$/
+const lineSplitRegex = /[\r\n]+/
+const targetsSplitRegex = /\s*,\s*/
+
+export function parse(input: string): AliasesFile | ParseError {
+  
+  const aliasesOrErrors: (Alias|ParseError)[] = input
+    .split(lineSplitRegex)
+    .filter(line => line !== "")
+    .map((line: string) => {
+      let match = line.match(aliasRegex)
+      if (match == null) {
+        if (line.match(commentRegex)) {
+          return null
+        } else {
+          return new ParseError(`Unrecognized aliases file line: ${line}`)
+        }
+      } else {
+        const [, localEmail, targetsPart] = match
+        const targets = targetsPart.split(targetsSplitRegex)
+        return { localEmail, targets }
+      }
+    })
+    .filter((x: Alias|ParseError|null) => x != null)
+  
+  const errors: ParseError[] = aliasesOrErrors.filter((x: ParseError|Alias) => x instanceof(ParseError)) as ParseError[]
+  
+  if (errors.length > 0) {
+    return errors[0]
+  } else {
+    const aliases: Alias[] = aliasesOrErrors.filter((x: ParseError|Alias) => !(x instanceof(ParseError))) as Alias[]
+    return {aliases}
+  }
 }
