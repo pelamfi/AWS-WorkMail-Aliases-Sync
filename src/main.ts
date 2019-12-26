f5ce3424789f6a8879ada0cb28f7c9f470ab6a83
Peter Lamberg <pgithub@pelam.fi>
2019-12-26 12:45:16 +0200

    problem: "Maximum alias limit exceeded" (100)

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,19 @@ import * as AliasesFileParse from '../src/AliasesFileParse';
 import * as Alias from '../src/Alias';
 import { readFileSync } from 'fs';
 import * as R from 'ramda';
+import {emailDomain, emailLocal} from '../src/EmailUtil'
 
 console.log("Script starting, configuring AWS");
 
 AWS.config.setPromisesDependency(null);
 AWS.config.loadFromPath("./aws-sdk-config.json")
-const workmail = new AWS.WorkMail({ endpoint: "https://workmail.eu-west-1.amazonaws.com" });
+let workmail = new AWS.WorkMail({ endpoint: "https://workmail.eu-west-1.amazonaws.com" });
 
-const scriptConfig = ScriptConfig.load()
+let scriptConfig = ScriptConfig.load()
 
 export function aliasesFromFile(): Alias.AliasesFile {
   console.log(`Parsing file: ${scriptConfig.aliasesFile}`)
-  const result = AliasesFileParse.parse(readFileSync(scriptConfig.aliasesFile).toString())
+  let result = AliasesFileParse.parse(readFileSync(scriptConfig.aliasesFile).toString())
   if (result instanceof AliasesFileParse.ParseError) {
     throw `Error parsing ${scriptConfig.aliasesFile}: ${result.error}`
   } else {
@@ -24,33 +25,60 @@ export function aliasesFromFile(): Alias.AliasesFile {
 }
 
 async function main() {
-  const aliases = aliasesFromFile()
-  const aliasesPerUser = Alias.aliasesPerUser(aliases)
+  let aliases = aliasesFromFile().aliases.filter(alias => alias.localEmails.length == 1)
+  let groups = aliasesFromFile().aliases.filter(alias => alias.localEmails.length > 1)
+  let aliasesPerUser = Alias.aliasesPerUser(aliases).users
 
-  console.log("Reading users and aliases from AWS WorkMail")
+  console.log(`Syncing users and aliases from with AWS WorkMail:\n` + 
+    `  Using configuration file: ${ScriptConfig.configFile}\n` +
+    `  WorkMail organizationId: ${scriptConfig.workmailOrganizationId}\n` +
+    `  aliases file to sync with: ${scriptConfig.aliasesFileDomain}\n` +
+    `  domain: ${scriptConfig.aliasesFileDomain}`)
 
-  const users = await workmail.listUsers({ OrganizationId: scriptConfig.workmailOrganizationId }).promise()
+  let currentUsersResponse = await workmail.listUsers({ OrganizationId: scriptConfig.workmailOrganizationId }).promise()
+  let currentGroupsResponse = await workmail.listGroups({ OrganizationId: scriptConfig.workmailOrganizationId }).promise()
+  let currentGroups = currentGroupsResponse.Groups
+  let currentUsers = currentUsersResponse.Users
 
-  users.Users.forEach(async (user) => {
+  currentUsers.forEach(async (user) => {
     let localUser = scriptConfig.emailToLocalEmail[user.Email]
     if (localUser == undefined) {
-      console.log(`Ignoring user ${user.Email} ${user.Name} that is not present in emailToLocalEmail dictionary in ${ScriptConfig.configFile}`)
+      console.log(`Ignoring user ${user.Email} ${user.Name} that is not present in emailToLocalEmail dictionary in configuration file`)
     } else {
 
-      const fileAliases = aliasesPerUser.users.find(aliases => aliases.localEmail == localUser)
-    
+      let fileAliases = aliasesPerUser.find(aliases => aliases.localEmail == localUser)
+      
+      function aliasToEmail(alias: string): string { return `${alias}@${scriptConfig.aliasesFileDomain}` }
+
       if (fileAliases == undefined) {
-        console.log("  No aliases defined in aliases file for ${localUser}. skipping")
+        console.log(`  No aliases defined in aliases file for ${localUser}. skipping`)
       } else {
-        const currentAliasesResponse = await workmail.listAliases({ EntityId: user.Id, OrganizationId: scriptConfig.workmailOrganizationId }).promise()
-        const currentAliases = currentAliasesResponse.Aliases.sort()
-        const fileAliasesThatDontExist = R.difference(fileAliases.aliases, currentAliases).sort()
-        const fileAliasesThatExist = R.intersection(fileAliases.aliases, currentAliases).sort()
-        const aliasesThatDontExistInFile = R.difference(currentAliases, fileAliases.aliases).sort()
+        let fileAliasesAsEmails = fileAliases.aliases.map(aliasToEmail)
+        let currentAliasesResponse = await workmail.listAliases({ EntityId: user.Id, OrganizationId: scriptConfig.workmailOrganizationId }).promise()
+
+        let currentAliases = currentAliasesResponse.Aliases.filter(alias => emailDomain(alias) == scriptConfig.aliasesFileDomain).sort()
+        let aliasesToAdd = R.difference(fileAliasesAsEmails, currentAliases).sort()
+        let okAliases = R.intersection(fileAliasesAsEmails, currentAliases).sort()
+        let aliasesToRemove = R.difference(currentAliases, fileAliasesAsEmails).sort()
+
         console.log(`user ${user.Email} ${user.Name} is mapped to ${localUser} in ${ScriptConfig.configFile}`)
-        console.log(`  aliases to be added: ${fileAliasesThatDontExist.join(",")}`)
-        console.log(`  aliases to be removed: ${aliasesThatDontExistInFile.join(",")}`)
-        console.log(`  ok aliases: ${fileAliasesThatExist.join(",")}`)
+        console.log(`  aliases to be added (${aliasesToAdd.length}): ${aliasesToAdd.map(emailLocal).join(", ")}`)
+        console.log(`  aliases to be removed (${aliasesToRemove.length}): ${aliasesToRemove.map(emailLocal).join(", ")}`)
+        console.log(`  ok aliases ${okAliases.length}`)
+
+        let initial = Promise.resolve<void>(undefined)
+
+        let addAliasesRequests: Promise<void> =
+          aliasesToAdd
+          .map(alias => {
+            let request = {OrganizationId: scriptConfig.workmailOrganizationId, EntityId: user.Id, Alias: alias}
+            return workmail.createAlias(request)
+          })
+          .reduce((a, b) => a.then(() => b.promise().then(_ => undefined)), initial)
+          
+        let chainedAdds = addAliasesRequests
+        await chainedAdds.catch(error => console.log(`Error adding alias to user ${user.Email}: ${error}`))
+        
       }
 
 
