b7ecc801770ea06f9d1ea1c83e9895be31f2e17f
Peter Lamberg <pgithub@pelam.fi>
2020-01-01 18:55:58 +0200

    WIP: 2 maps and diff to operations approach

--- src/AliasesFileToAwsMap.ts
+++ src/AliasesFileToAwsMap.ts
@@ -3,17 +3,18 @@ import {AliasesFileUsers} from './AliasesFile';
 import {AwsEmailMap, AwsUserAlias, AwsEmail} from './AwsEmail';
 import {emailAddDomain} from '../src/EmailUtil'
 
-export default function aliasesFileToAwsMap(aliasesFileUsers: AliasesFileUsers, aliasesFileDomain: string, localUserToAwsUser: { [index: string]: AWS.WorkMail.User }): AwsEmailMap {
+export default function aliasesFileToAwsMap(aliasesFileUsers: AliasesFileUsers, aliasesFileDomain: string, localUserToEntityId: ((localEmail: string) => AWS.WorkMail.WorkMailIdentifier|undefined)): AwsEmailMap {
   // TODO: Handle more than 100 aliases by creating groups
   let allAliases = R.flatten(aliasesFileUsers.users.map((localUser): AwsUserAlias[] => {
-    if (!(localUser.localEmail in localUserToAwsUser)) {
+    let userEntityId = localUserToEntityId(localUser.localEmail)
+    if (userEntityId === null) {
       throw `No AWS user for local user ${localUser.localEmail}`
+    } else {
+      return localUser.aliases.map( (alias): AwsUserAlias  => {
+        let email = emailAddDomain(alias, aliasesFileDomain)
+        return {kind: "AwsUserAlias", userEntityId: userEntityId || "", email}
+      })
     }
-    let user = localUserToAwsUser[localUser.localEmail]
-    return localUser.aliases.map( (alias): AwsUserAlias  => {
-      let email = emailAddDomain(alias, aliasesFileDomain)
-      return {kind: "AwsUserAlias", user, email}
-    })
   }))
 
   let allAliasesByEmail = R.groupBy((alias) => alias.email, allAliases)
