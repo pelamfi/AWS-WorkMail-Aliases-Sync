0f68e4866c7f33c3cd9c72a196ce39cef0afdbb1
Peter Lamberg <pgithub@pelam.fi>
2020-01-02 10:03:08 +0200

    WIP: 2 maps and diff to operations approach
    Now there are 4 simple tests

--- __tests__/AliasesFileToAwsMap.spec.ts
+++ __tests__/AliasesFileToAwsMap.spec.ts
@@ -22,4 +22,16 @@ describe('Creating AwsEmailMap', () => {
       });
   });
 
+  it('creates aliases for a 2 users', () => {
+    expect(aliasesFileToAwsMap({ users: [
+      { localEmail: "localemail", aliases: ["fooalias"] },
+      { localEmail: "localemail2", aliases: ["fooalias2", "fooalias3"] },
+      ] }, "domain", localUserToEntityId))
+      .toStrictEqual({
+        "fooalias@domain": { kind: "AwsUserAlias", userEntityId: "localemail-entityId", email: "fooalias@domain" },
+        "fooalias2@domain": { kind: "AwsUserAlias", userEntityId: "localemail2-entityId", email: "fooalias2@domain" },
+        "fooalias3@domain": { kind: "AwsUserAlias", userEntityId: "localemail2-entityId", email: "fooalias3@domain" }
+      });
+  });
+
 })
