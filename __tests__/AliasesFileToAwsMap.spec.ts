58961ad23b969f136ffa718d9ba7037c3295f3a3
Peter Lamberg <pgithub@pelam.fi>
2020-01-02 09:54:02 +0200

    WIP: 2 maps and diff to operations approach
    Add a simple test for target map creation.

--- __tests__/AliasesFileToAwsMap.spec.ts
+++ __tests__/AliasesFileToAwsMap.spec.ts
@@ -2,6 +2,10 @@ import { aliasesFileToAwsMap } from '../src/AliasesFileToAwsMap';
 
 describe('Creating AwsEmailMap', () => {
   it('accepts empty data', () => {
-    expect(aliasesFileToAwsMap({users: []}, "domain", () => "")).toStrictEqual({ });
+    expect(aliasesFileToAwsMap({ users: [] }, "domain", () => "")).toStrictEqual({});
+  });
+  it('creates a single alias', () => {
+    expect(aliasesFileToAwsMap({ users: [{ localEmail: "localemail", aliases: ["fooalias"] }] }, "domain", () => "entityid"))
+      .toStrictEqual({ "fooalias@domain": { kind: "AwsUserAlias", userEntityId: "entityid", email: "fooalias@domain" } });
   });
 })
