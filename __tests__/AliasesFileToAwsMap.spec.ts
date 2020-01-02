790986f0492485e19e49852727cf2401fd5de86d
Peter Lamberg <pgithub@pelam.fi>
2020-01-02 10:00:00 +0200

    WIP: 2 maps and diff to operations approach
    Add another simple test

--- __tests__/AliasesFileToAwsMap.spec.ts
+++ __tests__/AliasesFileToAwsMap.spec.ts
@@ -1,11 +1,25 @@
 import { aliasesFileToAwsMap } from '../src/AliasesFileToAwsMap';
 
+function localUserToEntityId(localEmail: string): AWS.WorkMail.WorkMailIdentifier {
+  return `${localEmail}-entityId`
+}
+
 describe('Creating AwsEmailMap', () => {
   it('accepts empty data', () => {
     expect(aliasesFileToAwsMap({ users: [] }, "domain", () => "")).toStrictEqual({});
   });
+
   it('creates a single alias', () => {
-    expect(aliasesFileToAwsMap({ users: [{ localEmail: "localemail", aliases: ["fooalias"] }] }, "domain", () => "entityid"))
-      .toStrictEqual({ "fooalias@domain": { kind: "AwsUserAlias", userEntityId: "entityid", email: "fooalias@domain" } });
+    expect(aliasesFileToAwsMap({ users: [{ localEmail: "localemail", aliases: ["fooalias"] }] }, "domain", localUserToEntityId))
+      .toStrictEqual({ "fooalias@domain": { kind: "AwsUserAlias", userEntityId: "localemail-entityId", email: "fooalias@domain" } });
   });
+
+  it('creates 2 aliases for a single user', () => {
+    expect(aliasesFileToAwsMap({ users: [{ localEmail: "localemail", aliases: ["fooalias", "fooalias2"] }] }, "domain", localUserToEntityId))
+      .toStrictEqual({
+        "fooalias@domain": { kind: "AwsUserAlias", userEntityId: "localemail-entityId", email: "fooalias@domain" },
+        "fooalias2@domain": { kind: "AwsUserAlias", userEntityId: "localemail-entityId", email: "fooalias2@domain" }
+      });
+  });
+
 })
