5523a3b469e72b76d8b082c15726f228fad1b1b0
Peter Lamberg <pgithub@pelam.fi>
2020-01-06 17:17:16 +0200

    AwsEmailExecute.ts first code

---
+++ src/AwsEmailExecute.ts
@@ -0,0 +1,25 @@
+import * as AWS from 'aws-sdk'
+import * as R from 'ramda';
+import {Workmail} from './AwsWorkMailUtil';
+import {AwsEmailOperation} from './AwsEmailOperation';
+
+export function executeAwsEmailOperation(workmail: Workmail, op: AwsEmailOperation): AWS.Request<any, AWS.AWSError> {
+  switch (op.kind) {
+    case "AddGroupAlias": {
+      let request = {OrganizationId: workmail.organizationId, EntityId: op.groupEntityId, Alias: op.aliasEmail}
+      return workmail.service.createAlias(request)
+    }
+    case "AddUserAlias": {
+      let request = {OrganizationId: workmail.organizationId, EntityId: op.userEntityId, Alias: op.aliasEmail}
+      return workmail.service.createAlias(request)
+    }
+    case "RemoveGroupAlias": {
+      let request = {OrganizationId: workmail.organizationId, EntityId: op.groupEntityId, Alias: op.aliasEmail}
+      return workmail.service.deleteAlias(request)
+    }
+    case "RemoveUserAlias": {
+      let request = {OrganizationId: workmail.organizationId, EntityId: op.userEntityId, Alias: op.aliasEmail}
+      return workmail.service.deleteAlias(request)
+    }
+  }
+}
