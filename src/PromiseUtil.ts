75ab488792b9808925d028a06a3d17fc7b6ca505
Peter Lamberg <pgithub@pelam.fi>
2020-01-09 09:13:52 +0200

    PromiseUtil hacking, flattenRight test

--- src/PromiseUtil.ts
+++ src/PromiseUtil.ts
@@ -1,14 +1,14 @@
 import * as R from 'ramda';
 
-export function serialPromisesReduce<T, O>(promises: (() => Promise<T>)[], initial: O, reduce: ((next:T, prev:O) => O)): Promise<O> {
+export function serialPromisesReduce<T, O>(promises: (() => Promise<T>)[], initial: O, reduce: ((prev:O, next:T) => O)): Promise<O> {
   let initialPromise: Promise<O> = Promise.resolve<O>(initial)
   function reductionStep(a: Promise<O>, b: () => Promise<T>): Promise<O> {
-    return a.then(prev => b().then(next => reduce(next, prev)))
+    return a.then(prev => b().then(next => reduce(prev, next)))
   }
   return promises.reduce(reductionStep, initialPromise)
 }
 
-function flattenRight<T>(array: [T, any[]]): T[] {
+export function flattenRight<T>(array: [T, any[]]): T[] {
   var result = [] as T[]
   while (array !== undefined) {
     result.push(array[0] as T)
@@ -20,7 +20,7 @@ function flattenRight<T>(array: [T, any[]]): T[] {
 export function serialPromises<T>(promises: (() => Promise<T>)[]): Promise<T[]> {
   // Collect in nested lists in a head growing linked list, where promise resolved last is the first element of first list.
   // This prevents N^2 behavior.
-  let accumulated: Promise<any[]> = serialPromisesReduce(promises, [] as T[], (prev, next) => R.append(prev, next))
+  let accumulated: Promise<any[]> = serialPromisesReduce(promises, [] as T[], (prev, next) => R.append(next, prev))
   // Flatten and reverse the list so promise resolved first is first in array.
   return accumulated.then(R.pipe(flattenRight, x => R.reverse(x))) as Promise<T[]>
 }
