5c9026a1a2d955bd4f10dee3a3d5520388c04098
Peter Lamberg <pgithub@pelam.fi>
2020-01-12 15:33:58 +0200

    Ignore unknown local users

--- src/AliasesFileToAwsMap.ts
+++ src/AliasesFileToAwsMap.ts
@@ -3,15 +3,17 @@ import {AliasesFileUsers} from './AliasesFile';
 import {AliasesFileUser} from './AliasesFile';
 import {WorkmailEmailmap, AwsUserAlias} from './AwsEmailMap';
 import {emailAddDomain} from './EmailUtil'
+import {filterUndef} from './UndefUtil'
 
 export function aliasesFileToAwsMap(aliasesFileUsers: AliasesFileUsers, aliasesFileDomain: string, localUserToEntityId: ((localEmail: string) => AWS.WorkMail.WorkMailIdentifier|undefined)): WorkmailEmailmap {
 
   // TODO: Handle more than 100 aliases by creating groups
 
-  function localUserToAwsAlias(localUser: AliasesFileUser): AwsUserAlias[] {
+  function localUserToAwsAlias(localUser: AliasesFileUser): AwsUserAlias[]|undefined {
     let userEntityId = localUserToEntityId(localUser.localEmail)
-    if (userEntityId === null) {
-      throw `No AWS user for local user ${localUser.localEmail}`
+    if (userEntityId === undefined) {
+      console.log(`Local email user '${localUser.localEmail}' is not in the configuration file localEmailUserToEmail map. Ignored.`)
+      return undefined
     } else {
       return localUser.aliases.map( (alias): AwsUserAlias  => {
         let email = emailAddDomain(alias, aliasesFileDomain)
@@ -20,13 +22,13 @@ export function aliasesFileToAwsMap(aliasesFileUsers: AliasesFileUsers, aliasesF
     }
   }
 
-  let allAliases = R.flatten(aliasesFileUsers.users.map(localUserToAwsAlias))
+  let allAliases = filterUndef(R.flatten(aliasesFileUsers.users.map(localUserToAwsAlias)))
 
   let allAliasesByEmail = R.groupBy((alias) => alias.email, allAliases)
 
   let [groups, regularAliases] = R.partition(alias => allAliasesByEmail[alias.email].length > 1, allAliases)
 
-  console.log(groups) // TODO: Handle cases where multiple users have same alias, create groups
+  console.log("TODO, handle groups", groups.length) // TODO: Handle cases where multiple users have same alias, create groups
 
   return R.zipObj(regularAliases.map(a => a.email), regularAliases)
 }
