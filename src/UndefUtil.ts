25a4535333686ac75ad6f4b27ef827f45ccb21aa
Peter Lamberg <pgithub@pelam.fi>
2020-01-12 08:59:52 +0200

    Add mapUndef to UndefUtil

--- src/UndefUtil.ts
+++ src/UndefUtil.ts
@@ -2,3 +2,12 @@
 export function filterUndef<T>(ts: (T | undefined)[]): T[] {
   return ts.filter((t: T | undefined): t is T => !!t)
 }
+
+// There isn't a ready made function like this? Probably is, but I can't find it
+export function mapUndef<T, U>(f: (value: T) => U, x: T | undefined): U | undefined {
+  if (x === undefined) {
+    return undefined
+  } else {
+    return f(x)
+  }
+}
