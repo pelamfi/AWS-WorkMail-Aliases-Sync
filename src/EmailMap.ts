6ea2fb0b15dd28aac611ff5dc9f541dbbaf6a4dc
Peter Lamberg <pgithub@pelam.fi>
2020-01-25 10:47:44 +0200

    Introduce the new no entityIds EmailMap

---
+++ src/EmailMap.ts
@@ -0,0 +1,38 @@
+
+export type PlainEmailAddress = string
+
+export class EmailAddr {
+  private _email: PlainEmailAddress // using private disables structural
+  constructor(email: PlainEmailAddress) {
+    this._email = email
+  }
+  get email() {
+    return this._email
+  }
+}
+
+export interface EmailUser {
+  readonly kind: "EmailUser",
+  readonly email: EmailAddr
+}
+
+export interface EmailUserAlias {
+  readonly kind: "EmailUserAlias"
+  readonly user: EmailUser,
+  readonly email: EmailAddr
+}
+
+export interface EmailGroup {
+  readonly kind: "EmailGroup",
+  readonly email: EmailAddr
+}
+
+export interface EmailGroupAlias {
+  readonly kind: "EmailGroupAlias",
+  readonly group: EmailGroup,
+  readonly email: EmailAddr
+}
+
+export type Email = EmailUser | EmailUserAlias | EmailGroup | EmailGroupAlias
+
+export type EmailMap = {[index: string]: Email}
