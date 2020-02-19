a6f5ede6b2245133e83cee3776fcd0c89c18718d
Peter Lamberg <pgithub@pelam.fi>
2020-02-19 13:31:39 +0200

    Handle changing groups

--- __tests__/AwsMapSync.spec.ts
+++ __tests__/AwsMapSync.spec.ts
@@ -1,5 +1,5 @@
 import { awsMapSync } from '../src/AwsMapSync';
-import { EmailMap, EmailUser } from '../src/EmailMap';
+import { EmailMap, EmailUser, EmailGroup } from '../src/EmailMap';
 import { EmailOperation } from '../src/EmailOperation';
 import { EmailAddr } from '../src/EmailAddr';
 
@@ -48,6 +48,22 @@ describe('Synchronizing 2 AwsMaps', () => {
     const target: EmailMap = {email1: {kind: "EmailUserAlias", email: email1, user: user1}}
     const expected: EmailOperation[] = []
     expect(awsMapSync(current, target)).toStrictEqual(expected);
+  });
+
+  it('Does not do anything if group is not changed', () => {
+    const current: EmailMap = {email1: {kind: "EmailGroup", email: email1, name: "foogroup", members: [user1]}}
+    const target: EmailMap = {email1: {kind: "EmailGroup", email: email1, name: "foogroup", members: [user1]}}
+    const expected: EmailOperation[] = []
+    expect(awsMapSync(current, target)).toStrictEqual(expected)
+  });
+
+  it('Removes and recreates a group if members have changed', () => {
+    const group1: EmailGroup = {kind: "EmailGroup", email: email1, name: "foogroup", members: [user1]}
+    const group1b: EmailGroup =  {kind: "EmailGroup", email: email1, name: "foogroup", members: [user1, user2]}
+    const current: EmailMap = {email1: group1}
+    const target: EmailMap = {email1: group1b}
+    const expected: EmailOperation[] = [{kind: "RemoveGroup", group: group1 }, {kind: "AddGroup", group: group1b}, {kind: "AddGroupMember", group: group1b, member: user1}, {kind: "AddGroupMember", group: group1b, member: user2}]  
+    expect(awsMapSync(current, target)).toStrictEqual(expected)
   });
 
 })
--- src/AwsMapSync.ts
+++ src/AwsMapSync.ts
@@ -3,6 +3,12 @@ import * as R from 'ramda';
 import {filterUndef} from './UndefUtil'
 import { EmailMap, EmailGroup } from './EmailMap';
 
+function groupsEqual(a: EmailGroup, b: EmailGroup): boolean {
+  let aMemberEmails = a.members.map(x=>x.email.email).sort()
+  let bMemberEmails = b.members.map(x=>x.email.email).sort()
+  return a.email.email == b.email.email && a.name == b.name && R.equals(aMemberEmails, bMemberEmails)
+}
+
 export function awsMapSync(currentMap: EmailMap, targetMap: EmailMap): EmailOperation[] {
 
   const removals = R.keys(currentMap).map((email): EmailOperation|undefined => {
@@ -16,8 +22,8 @@ export function awsMapSync(currentMap: EmailMap, targetMap: EmailMap): EmailOper
     if (current.kind == "EmailGroupAlias" && ((target?.kind == "EmailGroupAlias" && target?.group.email.email != current.group.email.email) || current.kind != target?.kind)) {
       return {kind: "RemoveGroupAlias", alias: current}
     }
-    else if (current.kind == "EmailGroup" && target !== undefined && current.kind != target.kind) {
-      throw `Email ${current.email} is configured as ${current.kind}. Removing/changing the group is currently not supported. Please fix manually. It is expected to be ${target.kind} ${target.email}` // can this happen?
+    else if (current.kind == "EmailGroup" && target !== undefined && (target.kind !== "EmailGroup" || !groupsEqual(current, target))) {
+      return {kind: "RemoveGroup", group: current}
     }
     else if (current.kind == "EmailUserAlias" && ((target?.kind == "EmailUserAlias" && target?.user.email.email != current.user.email.email) || current.kind != target?.kind)) {
       return {kind: "RemoveUserAlias", alias: current}
@@ -44,7 +50,7 @@ export function awsMapSync(currentMap: EmailMap, targetMap: EmailMap): EmailOper
       }
       break;
       case "EmailGroup": 
-      if (current == undefined) {
+      if (current == undefined || (current.kind !== "EmailGroup" || !groupsEqual(current, target))) {
         const group: EmailGroup = target
         const members: AddGroupMember[] = target.members.map(member => ({kind: "AddGroupMember", group, member}))
         return [{kind: "AddGroup", group}, ...members]
