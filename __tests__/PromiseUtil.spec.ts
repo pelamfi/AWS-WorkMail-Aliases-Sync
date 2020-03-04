a61d8bf1eb9c9cb55d3ec5f38cf7f164ab9bcd7f
Peter Lamberg <pgithub@pelam.fi>
2020-03-04 08:27:45 +0200

    Remove PromiseUtil.ts experiments

--- __tests__/PromiseUtil.spec.ts
+++ __tests__/PromiseUtil.spec.ts
@@ -1,35 +1,12 @@
-import { serialPromisesFlatten, flattenRight } from '../src/PromiseUtil';
-import * as R from 'ramda';
+import { serialPromises } from '../src/PromiseUtil';
 
 describe('Promise utilities', () => {
-  it('accepts empty list', () => {
-    return serialPromisesFlatten([]).then(result => expect(result).toStrictEqual([]));
+  it('serialPromises accepts empty list', () => {
+    return serialPromises([]).then(result => expect(result).toStrictEqual([]));
   });
-
-  it('flattenRight flattens 3 levels', () => {
-    return expect(flattenRight(['a', ['b', ['c']]])).toStrictEqual(['a', 'b', 'c'])
-  });
-
-  it('flattenRight accepts empty list', () => {
-    return expect(flattenRight([])).toStrictEqual([])
-  });
-
-  it('flattenRight accepts list of 1', () => {
-    return expect(flattenRight(["foo"])).toStrictEqual(["foo"])
-  });
-
-  it('flattenRight accepts list of 1 + empty', () => {
-    return expect(flattenRight(["foo", []])).toStrictEqual(["foo"])
-  });
-
-  it('flattenRight accepts a large tree', () => {
-    const array: number[] = Array(100000).map((_, i) => i)
-    const tree = R.reduce((prev, x) => [x, prev], [], array) as any[]
-    return expect(flattenRight(tree).length).toStrictEqual(array.length)
-  });
-
-  it('accepts one', () => {
-    return serialPromisesFlatten([() => Promise.resolve(["foo"])]).then(result => expect(result).toStrictEqual(["foo"]));
+  
+  it('serialPromises accepts one', () => {
+    return serialPromises([() => Promise.resolve("foo")]).then(result => expect(result).toStrictEqual(["foo"]));
   });
 
 });
