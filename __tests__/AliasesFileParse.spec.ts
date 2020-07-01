4582102723370eee0992ccb2c687b16830540a20
Peter Lamberg <pgithub@pelam.fi>
2020-07-01 12:45:24 +0300

    Fix related to previous refactoring

--- __tests__/AliasesFileParse.spec.ts
+++ __tests__/AliasesFileParse.spec.ts
@@ -1,48 +1,48 @@
-import { parse, ParseError } from '../src/AliasesFileParse';
+import { parseAliasesFile, AliasesFileParseError } from '../src/AliasesFileParse';
 
 describe('Parsing aliases file', () => {
   it('accepts empty file', () => {
-    expect(parse('')).toStrictEqual({ aliases: [] });
+    expect(parseAliasesFile('')).toStrictEqual({ aliases: [] });
   });
 
   it('accepts single alias', () => {
-    expect(parse('foo: bar')).toStrictEqual({
+    expect(parseAliasesFile('foo: bar')).toStrictEqual({
       aliases: [{ alias: 'foo', localEmails: ['bar'] }],
     });
   });
 
   it('skips comments and empty lines', () => {
-    expect(parse('#comment\nfoo: bar\n\n')).toStrictEqual({
+    expect(parseAliasesFile('#comment\nfoo: bar\n\n')).toStrictEqual({
       aliases: [{ alias: 'foo', localEmails: ['bar'] }],
     });
   });
 
   it('detects error', () => {
-    expect(parse('\nunknown stuff!!!\nfoo: bar\n\n')).toStrictEqual(
-      new ParseError('Unrecognized aliases file line: unknown stuff!!!'),
+    expect(parseAliasesFile('\nunknown stuff!!!\nfoo: bar\n\n')).toStrictEqual(
+      new AliasesFileParseError('Unrecognized aliases file line: unknown stuff!!!'),
     );
   });
 
   it('parses multiple targets', () => {
-    expect(parse('foo: bar,baz')).toStrictEqual({
+    expect(parseAliasesFile('foo: bar,baz')).toStrictEqual({
       aliases: [{ alias: 'foo', localEmails: ['bar', 'baz'] }],
     });
   });
 
   it('skips comments at end of line', () => {
-    expect(parse('foo: bar,baz#comment')).toStrictEqual({
+    expect(parseAliasesFile('foo: bar,baz#comment')).toStrictEqual({
       aliases: [{ alias: 'foo', localEmails: ['bar', 'baz'] }],
     });
   });
 
   it('skips comments and whitespace at end of line', () => {
-    expect(parse('foo: bar,baz #comment')).toStrictEqual({
+    expect(parseAliasesFile('foo: bar,baz #comment')).toStrictEqual({
       aliases: [{ alias: 'foo', localEmails: ['bar', 'baz'] }],
     });
   });
 
   it('skips comments that look like aliases', () => {
-    expect(parse('foo: bar,baz\n#a: b')).toStrictEqual({
+    expect(parseAliasesFile('foo: bar,baz\n#a: b')).toStrictEqual({
       aliases: [{ alias: 'foo', localEmails: ['bar', 'baz'] }],
     });
   });
